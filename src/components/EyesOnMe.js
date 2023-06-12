ea98bd06c92451e57c1053e390f4cc0b8e60b256
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-06-12 15:19:57 -0600

    lab completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+
+function focus() {
+  console.log('Good!');
+}
+
+function blur() {
+  console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe() {
+  return (
+    <div>
+      <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe;
